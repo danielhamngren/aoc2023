16cd270e3a2aa03426bfc9b690be62bdb7829ff3
Daniel Hamngren <daniel.hamngren@gmail.com>
2023-12-05 17:07:29 +0000

    Day 5 part 1

--- aoc/day5.go
+++ aoc/day5.go
@@ -3,6 +3,7 @@ package aoc
 import (
 	"fmt"
 	"log"
+	"math"
 	"os"
 	"regexp"
 	"strconv"
@@ -34,13 +35,13 @@ func parseInputDay5(input []string) ([]int, map[string]Function) {
 
 	var currentFunction Function
 	var currentFunctionName string
-	for _, line := range input {
+	for i, line := range input {
 		fmt.Println(line)
 		if strings.Contains(line, "map") {
-			currentFunctionName = line
+			currentFunctionName = strings.Split(line, " ")[0]
 			currentFunction = Function{[]FunctionSection{}}
 			continue
-		} else if line == "" {
+		} else if line == "" || i == len(input)-1 {
 			fmt.Println("add function to map")
 			m[currentFunctionName] = currentFunction
 			continue
@@ -59,6 +60,41 @@ func parseInputDay5(input []string) ([]int, map[string]Function) {
 	return seeds, m
 }
 
+func executeFunction(f Function, x int) int {
+	for _, section := range f.sections {
+		fmt.Println(section.startDestination, section.startSource, section.sectionRange, x)
+
+		if x >= section.startSource && x <= section.startSource+section.sectionRange {
+			fmt.Println("section valid")
+			return section.startDestination + x - section.startSource
+		}
+	}
+
+	return x
+}
+
+func executeMaps(functions map[string]Function, x int) int {
+	fmt.Println("seed:", x)
+	x = executeFunction(functions["seed-to-soil"], x)
+	fmt.Println("soil:", x)
+
+	x = executeFunction(functions["soil-to-fertilizer"], x)
+	fmt.Println("fert:", x)
+	x = executeFunction(functions["fertilizer-to-water"], x)
+	fmt.Println("water:", x)
+	x = executeFunction(functions["water-to-light"], x)
+	fmt.Println("light:", x)
+	x = executeFunction(functions["light-to-temperature"], x)
+	fmt.Println("temp:", x)
+	x = executeFunction(functions["temperature-to-humidity"], x)
+	fmt.Println("humidity:", x)
+	x = executeFunction(functions["humidity-to-location"], x)
+	fmt.Println("location:", x)
+
+	return x
+
+}
+
 func day5part1(input []string) int {
 	seeds, m := parseInputDay5(input)
 
@@ -66,8 +102,20 @@ func day5part1(input []string) int {
 
 	//TODO: find which seed has the lowest location.
 	//TODO: process the seed numbers using the output of the parse function
+	min_location := math.MaxInt64
+	min_seed := seeds[0]
 
-	result := 0
+	for _, seed := range seeds {
+		location := executeMaps(m, seed)
+		if location < min_location {
+			min_seed = seed
+			min_location = location
+		}
+	}
+
+	fmt.Println(min_seed, min_location)
+
+	result := min_location
 
 	return result
 }
@@ -129,7 +177,7 @@ func inputDay5(test bool) []string {
 
 func Day5() {
 
-	resultPart1 := day5part1(inputDay5(true))
+	resultPart1 := day5part1(inputDay5(false))
 	fmt.Println(resultPart1)
 	resultPart2 := day5part2(inputDay5(true))
 	fmt.Println(resultPart2)
