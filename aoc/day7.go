009adba3e9d51ebb3b89810024c076d2624a4f96
Daniel Hamngren <daniel.hamngren@gmail.com>
2023-12-07 20:41:08 +0000

    Day 7 part 2

--- aoc/day7.go
+++ aoc/day7.go
@@ -30,7 +30,7 @@ func parseInputDay7(input []string) []Hand {
 	return hands
 }
 
-func checkCards(cards string) int {
+func checkCards(cards string, jokers bool) int {
 	cardsMap := make(map[rune]int)
 
 	for _, c := range cards {
@@ -43,13 +43,30 @@ func checkCards(cards string) int {
 
 	}
 
+	jo := 0
+	if jokers {
+		var ok bool
+		jo, ok = cardsMap['J']
+		delete(cardsMap, 'J')
+		if !ok {
+			jo = 0
+		}
+
+	}
+
 	values := []int{}
 	for _, v := range cardsMap {
 		values = append(values, v)
 
 	}
+	if len(values) == 0 {
+		values = append(values, 0)
+	}
 	slices.SortFunc(values, func(i, j int) int { return j - i })
 	// Check 5 of a kind 7
+	fmt.Print(cardsMap, jo)
+	values[0] = values[0] + jo
+
 	if values[0] == 5 {
 		return 7
 	}
@@ -83,21 +100,41 @@ func checkCards(cards string) int {
 	return 1
 }
 
-func firstWins(x, y Hand) int {
-	values := map[byte]int{
-		'A': 14,
-		'K': 13,
-		'Q': 12,
-		'J': 11,
-		'T': 10,
-		'9': 9,
-		'8': 8,
-		'7': 7,
-		'6': 6,
-		'5': 5,
-		'4': 4,
-		'3': 3,
-		'2': 2,
+func firstWins(x, y Hand, jokers bool) int {
+	var values map[byte]int
+	if jokers {
+		values = map[byte]int{
+			'A': 14,
+			'K': 13,
+			'Q': 12,
+			'J': 1,
+			'T': 10,
+			'9': 9,
+			'8': 8,
+			'7': 7,
+			'6': 6,
+			'5': 5,
+			'4': 4,
+			'3': 3,
+			'2': 2,
+		}
+	} else {
+		values = map[byte]int{
+			'A': 14,
+			'K': 13,
+			'Q': 12,
+			'J': 11,
+			'T': 10,
+			'9': 9,
+			'8': 8,
+			'7': 7,
+			'6': 6,
+			'5': 5,
+			'4': 4,
+			'3': 3,
+			'2': 2,
+		}
+
 	}
 
 	for i := range x.cards {
@@ -111,14 +148,14 @@ func firstWins(x, y Hand) int {
 
 }
 
-func checkWinningHand(x, y Hand) int {
+func checkWinningHand(x, y Hand, jokers bool) int {
 	// 1. Get the hand type
-	handOrderX := checkCards(x.cards)
-	handOrderY := checkCards(y.cards)
+	handOrderX := checkCards(x.cards, jokers)
+	handOrderY := checkCards(y.cards, jokers)
 
 	if handOrderX == handOrderY {
 		// 3. If same, handle draw
-		return firstWins(x, y)
+		return firstWins(x, y, jokers)
 	}
 	// 2. Check which is better
 
@@ -132,7 +169,7 @@ func day7part1(input []string) int {
 
 	fmt.Println(hands)
 
-	slices.SortFunc(hands, func(i, j Hand) int { return checkWinningHand(i, j) })
+	slices.SortFunc(hands, func(i, j Hand) int { return checkWinningHand(i, j, false) })
 	fmt.Println(hands)
 
 	// get total winnings
@@ -145,7 +182,21 @@ func day7part1(input []string) int {
 }
 
 func day7part2(input []string) int {
-	return 0
+	hands := parseInputDay7(input)
+	result := 0
+
+	fmt.Println(hands)
+
+	slices.SortFunc(hands, func(i, j Hand) int { return checkWinningHand(i, j, true) })
+	fmt.Println(hands)
+
+	// get total winnings
+
+	for i, hand := range hands {
+		result += hand.bet * (i + 1)
+	}
+
+	return result
 }
 
 func inputDay7(test bool) []string {
@@ -171,10 +222,8 @@ func inputDay7(test bool) []string {
 }
 
 func Day7() {
-
 	resultPart1 := day7part1(inputDay7(false))
 	fmt.Println(resultPart1)
-	resultPart2 := day7part2(inputDay7(true))
+	resultPart2 := day7part2(inputDay7(false))
 	fmt.Println(resultPart2)
-
 }
